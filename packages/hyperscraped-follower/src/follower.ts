b0318bd477fe3d1a02f428e5a7bb82215a73a393
Gillis Van Ginderachter <supergillis@gmail.com>
2021-11-28 15:19:14 +0100

    Improve follower API

--- packages/hyperscraped-follower/src/follower.ts
+++ packages/hyperscraped-follower/src/follower.ts
@@ -1,31 +1,41 @@
-import fetch, { RequestInit } from 'node-fetch';
+import fetch, { Response, RequestInfo, RequestInit } from 'node-fetch';
 import type { Document } from 'domhandler';
 import { parseDocument } from 'htmlparser2';
 import * as TE from 'fp-ts/TaskEither';
 
-export const createRequester =
+export const createRawRequester =
   (init: RequestInit = { timeout: 10000 }) =>
-  (url: string): TE.TaskEither<any, Document> =>
+  (url: RequestInfo): TE.TaskEither<any, Response> =>
     TE.tryCatch(
-      async () => {
-        const response = await fetch(url, init);
-        const body = await response.text();
-        return parseDocument(body);
-      },
+      () => fetch(url, init),
       (error) => error,
     );
 
-export const requestT = createRequester();
+export const createTextRequester =
+  (init: RequestInit = { timeout: 10000 }) =>
+  (url: RequestInfo): TE.TaskEither<any, string> =>
+    TE.chainTaskK((response: Response) => () => response.text())(createRawRequester(init)(url));
+
+export const createDocumentRequester =
+  (init: RequestInit = { timeout: 10000 }) =>
+  (url: RequestInfo): TE.TaskEither<any, Document> =>
+    TE.chainTaskK((body: string) => async () => parseDocument(body))(createTextRequester(init)(url));
+
+/**
+ * @deprecated
+ */
+export const createRequester = createDocumentRequester;
 
 export const createJsonRequester =
   (init: RequestInit = { timeout: 10000 }) =>
-  (url: string): TE.TaskEither<any, any> =>
-    TE.tryCatch(
-      async () => {
-        const response = await fetch(url, init);
-        return await response.json();
-      },
-      (error) => error,
-    );
+  (url: RequestInfo): TE.TaskEither<any, any> =>
+    TE.chainTaskK((response: Response) => () => response.json())(createRawRequester(init)(url));
 
+export const requestRawT = createRawRequester();
+export const requestDocumentT = createDocumentRequester();
+/**
+ * @deprecated
+ */
+export const requestT = createRequester();
+export const requestTextT = createTextRequester();
 export const requestJsonT = createJsonRequester();
